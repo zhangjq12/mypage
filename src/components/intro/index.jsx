0a875a801bb46bb05182c5a3aa66f02646152946
zhangjq12 <524796875@qq.com>
2026-06-11 12:57:19 +0800

    image issues

--- src/components/intro/index.jsx
+++ src/components/intro/index.jsx
@@ -5,7 +5,7 @@ import { INTRO } from "../../i18n";
 export const Intro = ({ i18n }) => {
   return (
     <>
-      <Avatar size={120} src={"/avatar.jpg"} />
+      <Avatar size={120} src={`${process.env.PUBLIC_URL}/avatar.jpg`} />
       <h1>{INTRO[i18n]["name"]}</h1>
       <p>{INTRO[i18n]["degree"]}</p>
       <p>{INTRO[i18n]["school"]}</p>
